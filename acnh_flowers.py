128a8a7fb72db4866bc35592bc6c062130838726
zhongsh <zhong.sh@husky.neu.edu>
2020-05-25 17:23:37 -0400

    Change flower data link to online

--- acnh_flowers.py
+++ acnh_flowers.py
@@ -231,7 +231,7 @@ class FlowerColor:
     @classmethod
     def get_df(cls):
         if cls.__table is None:
-            cls.__table = read_csv('resources/flower_color.csv')
+            cls.__table = read_csv('https://raw.githubusercontent.com/zhongzachary/acnh_flowers/master/resources/flower_color.csv')
             cls.__table['species'] = cls.__table['species'].apply(lambda r: Species(r))
             cls.__table['gene_sequence'] = cls.__table.iloc[:, 1:5].apply(
                 lambda row: row.apply(lambda c: Gene.from_str(c) if type(c) is str else None), axis=1).apply(
